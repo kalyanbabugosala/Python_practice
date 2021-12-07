e0d1b57adbf280ab2815954dfaf6bfe223fd05d0
Kalyan <kalyanbabu.gosala@thundersoft.com>
2021-12-07 19:01:10 +0530

    created conditional statements

---
+++ Conditional_statements.py
@@ -0,0 +1,33 @@
+#conditional statements
+#x=0
+'''if x>0:
+    print("x is pisitive")
+    
+'''
+
+'''if x>0:
+    print("x is positive")
+else:
+    print("x is not positive")
+    
+'''
+
+'''x = int(input("Enter a number"))
+if(x>0):
+    print("x is positive")
+elif(x<0):
+    print("x is negative")
+else:
+    print("x is 0")
+
+'''
+
+x = int(input("Enter a number:"))
+if x>=0:
+    if x==0:
+        print("X is 0")
+    else:
+        print("x is positive")
+else:
+    print("x is negative")
+    
